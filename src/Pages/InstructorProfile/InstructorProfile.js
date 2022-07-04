24e7d241cb0174070b408cd56448f076483b4a63
sibteali786 <sibteali786@gmail.com>
2022-07-05 00:18:44 +0500

    implemented read more capability

--- src/Pages/InstructorProfile/InstructorProfile.js
+++ src/Pages/InstructorProfile/InstructorProfile.js
@@ -5,21 +5,21 @@ import {
   Typography,
   TextField,
   IconButton,
-  Box,
-  Grid
+  Grid,
+  Collapse,
 } from "@mui/material";
 import "./../../../node_modules/video-react/dist/video-react.css";
 import React, { useState } from "react";
 import "./InstructorProfile.scss";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
+// All the images in the page used 
 import profileImage from "../../Assets/Profiles/Profile Pic.png";
 import instructorImages from "../../Assets/Profiles/instructorImages";
 import Rectangle5 from "../../Assets/Profiles/Rectangle 5.png";
 import Rectangle6 from "../../Assets/Profiles/Rectangle 6.png";
 import Rectangle7 from "../../Assets/Profiles/Rectangle 7.png";
 import Rectangle8 from "../../Assets/Profiles/Rectangle 8.png";
-import RemoveIcon from "@mui/icons-material/Remove";
 import AddIcon from "@mui/icons-material/Add";
 import { Link } from "react-router-dom";
 import { Player, BigPlayButton } from "video-react";
@@ -37,43 +37,52 @@ const InstructorProfile = () => {
       setInputFields([...inputFields, { inputValue }]);
     }
   };
-  const handleRemoveFields = (index) => {
-    const values = [...inputFields];
-    values.splice(index, 1);
-    setInputFields(values);
+  // For collapsing the read more panel 
+  const [checked, setChecked] = React.useState(false);
+
+  const handleChange = () => {
+    setChecked((prev) => !prev);
   };
   return (
     <div className="Instrcutor-container">
       <div className="backgroundPicture"></div>
-      <Container maxWidth="md">
-        <Grid container spacing={2} alignItems="center" justifyContent="space-evenly">
-        <Grid item >
-        <ButtonBase
-          sx={{
-            width: 150,
-            height: 150,
-            position: "relative",
-            top: "-10vh",
-            left: "-2vw",
-          }}
-          >
-          <img alt="complex" src={profileImage} style={{ width: "100%" }} />
-        </ButtonBase>
-        </Grid>
-        <Grid item className="instName">
-        <h3>John Doe</h3>
-        <a href="email:johnDoe">@johndoe</a>
-        <p>Astrophotographer 🌌</p>
-        <p>Gamer 👾</p>
-        </Grid>
-        <Button
-          variant="contained"
-          style={{ backgroundColor: "#196AA0", borderRadius: "1.3rem" }}
-          >
-          Follow
-        </Button>
+      <Container maxWidth="lg" className="grids">
+        <Grid
+          container
+          spacing={2}
+          alignItems="flex-start"
+          justifyContent="space-between"
+          style={{ marginTop: "0px" }}
+          className="span-1"
+        >
+          <Grid item className="instName">
+            <ButtonBase
+              sx={{
+                width: 150,
+                height: 150,
+                position: "relative",
+                top: "-10vh",
+              }}
+            >
+              <img alt="complex" src={profileImage} style={{ width: "100%" }} />
+            </ButtonBase>
+            <Grid item style={{ marginLeft: "2rem" }}>
+              <h3>John Doe</h3>
+              <a href="email:johnDoe">@johndoe</a>
+              <p>Astrophotographer 🌌</p>
+              <p>Gamer 👾</p>
+            </Grid>
           </Grid>
-        <Container maxWidth="sm">
+          <Grid item>
+            <Button
+              variant="contained"
+              style={{ backgroundColor: "#196AA0", borderRadius: "1.3rem" }}
+            >
+              Follow
+            </Button>
+          </Grid>
+        </Grid>
+        <Container maxWidth="sm" className="span-2">
           <h4>Photos</h4>
           <ImageList sx={{ width: 500, height: 450 }} cols={2} rowHeight={164}>
             {instructorImages.map((item) => (
@@ -90,139 +99,142 @@ const InstructorProfile = () => {
           <Button
             variant="text"
             style={{ fontWeight: "bold", color: "#196AA0" }}
-            >
+          >
             More +
           </Button>
         </Container>
-      </Container>
-      <Container
-        maxWidth="md"
-        style={{ backgroundColor: "#F1F1F1", borderRadius: "1rem" }}
-      >
-        <h2>How Astrophotography changed my life</h2>
-        <Typography variant="body2">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
-          turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec
-          fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus
-          elit sed risus. Maecenas eget condimentum velit, sit amet feugiat
-          lectus. Class aptent taciti sociosqu ad litora torquent per conubia
-          nostra, per inceptos himenaeos. Praesent auctor purus luctus enim
-          egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex.
-          Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum
-          lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in
-          elementum tellus. Curabitur tempor quis eros tempus lacinia. Nam
-          bibendum pellentesque quam a convallis. Sed ut vulputate nisi. Integer
-          in felis sed leo vestibulum venenatis. Suspendisse quis arcu sem.
-          Aenean feugiat ex eu vestibulum vestibulum. Morbi a eleifend magna.
-          Nam metus lacus, porttitor eu mauris a, blandit ultrices nibh. Mauris
-          sit amet magna non ligula vestibulum eleifend. Nulla varius volutpat
-          turpis sed lacinia. Nam eget mi in purus lobortis eleifend. Sed nec
-          ante dictum sem condimentum ullamcorper quis venenatis nisi. Proin
-          vitae facilisis nisi, ac posuere leo.
-        </Typography>
-
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "flex-start",
-            alignItems: "center",
-            flexWrap: "wrap",
-          }}
+        <Container
+          maxWidth="md"
+          style={{ backgroundColor: "#F1F1F1", borderRadius: "1rem" }}
+          className="span-3"
         >
-          {inputFields.map((inputField, index) => (
-            <Button
-              key={index}
-              variant="contained"
+          <Collapse in={checked} collapsedSize={200}>
+
+          <h2>How Astrophotography changed my life</h2>
+          <Typography variant="body2">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
+            turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus
+            nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum
+            tellus elit sed risus. Maecenas eget condimentum velit, sit amet
+            feugiat lectus. Class aptent taciti sociosqu ad litora torquent per
+            conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus
+            enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex.
+            Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum
+            lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in
+            elementum tellus. Curabitur tempor quis eros tempus lacinia. Nam
+            bibendum pellentesque quam a convallis. Sed ut vulputate nisi.
+            Integer in felis sed leo vestibulum venenatis. Suspendisse quis arcu
+            sem. Aenean feugiat ex eu vestibulum vestibulum. Morbi a eleifend
+            magna. Nam metus lacus, porttitor eu mauris a, blandit ultrices
+            nibh. Mauris sit amet magna non ligula vestibulum eleifend. Nulla
+            varius volutpat turpis sed lacinia. Nam eget mi in purus lobortis
+            eleifend. Sed nec ante dictum sem condimentum ullamcorper quis
+            venenatis nisi. Proin vitae facilisis nisi, ac posuere leo.
+          </Typography>
+          </Collapse>
+
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "flex-start",
+              alignItems: "center",
+              flexWrap: "wrap",
+            }}
+          >
+            {inputFields.map((inputField, index) => (
+              <Button
+                key={index}
+                variant="contained"
+                size="small"
+                style={{
+                  color: "#76A4CE",
+                  backgroundColor: "#FFF",
+                  borderRadius: "1rem",
+                  margin: "0.2rem",
+                }}
+              >
+                {inputField.inputValue}
+              </Button>
+            ))}
+            <TextField
+              label="Skill"
+              onChange={(e) => {
+                setinputValue(e.target.value);
+              }}
               size="small"
+            />
+            <IconButton
+              onClick={() => handleAddFields()}
               style={{
                 color: "#76A4CE",
                 backgroundColor: "#FFF",
-                borderRadius: "1rem",
-                margin: "0.2rem",
+                borderRadius: "2rem",
+                margin: "0.5rem",
               }}
             >
-              {inputField.inputValue}
+              <AddIcon />
+            </IconButton>
+            <Button
+              variant="text"
+              style={{ fontWeight: "bold", color: "#196AA0" }}
+              onClick = {handleChange}
+            >
+              Read More
             </Button>
-          ))}
-          <TextField
-            label="Skill"
-            onChange={(e) => {
-              setinputValue(e.target.value);
-            }}
-            size="small"
-          />
-          <IconButton
-            onClick={() => handleAddFields()}
-            style={{
-              color: "#76A4CE",
-              backgroundColor: "#FFF",
-              borderRadius: "2rem",
-              margin: "0.5rem",
-            }}
-          >
-            <AddIcon />
-          </IconButton>
-          <Button
-            variant="text"
-            style={{ fontWeight: "bold", color: "#196AA0" }}
-          >
-            Read More
-          </Button>
-        </div>
-      </Container>
-      <Container maxWidth="lg">
-        <Link to="/profile" style={{ textDecoration: "none" }}>
-          <Button
-            variant="text"
-            style={{ fontWeight: "bold", color: "#196AA0" }}
-          >
-            More Posts by John Doe
-          </Button>
-        </Link>
-      </Container>
-      <Container maxWidth="sm">
-        <h2>Videos</h2>
-        <Player
-          playsInline
-          poster={Rectangle5}
-          src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
-        >
-          <BigPlayButton position="center" />
-        </Player>
-      </Container>
-      <Container maxWidth="md">
-        <h2>Offered Courses</h2>
-        <div style={{ display: "flex", direction: "row" }}>
-          <Container style={{ maxWidth: "250px" }}>
-            <Player
-              playsInline
-              poster={Rectangle7}
-              src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+          </div>
+          <Link to="/profile" style={{ textDecoration: "none" }}>
+            <Button
+              variant="text"
+              style={{ fontWeight: "bold", color: "#196AA0" }}
             >
-              <BigPlayButton position="center" />
-            </Player>
-          </Container>
-          <Container style={{ maxWidth: "250px" }}>
-            <Player
-              playsInline
-              poster={Rectangle6}
-              src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
-            >
-              <BigPlayButton position="center" />
-            </Player>
-          </Container>
-          <Container style={{ maxWidth: "250px" }}>
-            <Player
-              playsInline
-              poster={Rectangle8}
-              src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
-            >
-              <BigPlayButton position="center" />
-            </Player>
-          </Container>
-        </div>
-        <Container maxWidth="md">
+              More Posts by John Doe
+            </Button>
+          </Link>
+        </Container>
+        <Container maxWidth="sm" className="span-4">
+          <h2>Videos</h2>
+          <Player
+            playsInline
+            poster={Rectangle5}
+            src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+          >
+            <BigPlayButton position="center" />
+          </Player>
+        </Container>
+        <Container maxWidth="md" className="span-5">
+          <h2>Offered Courses</h2>
+          <div style={{ display: "flex", direction: "row" }}>
+            <Container style={{ maxWidth: "250px" }}>
+              <Player
+                playsInline
+                poster={Rectangle7}
+                src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+              >
+                <BigPlayButton position="center" />
+              </Player>
+            </Container>
+            <Container style={{ maxWidth: "250px" }}>
+              <Player
+                playsInline
+                poster={Rectangle6}
+                src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+              >
+                <BigPlayButton position="center" />
+              </Player>
+            </Container>
+            <Container style={{ maxWidth: "250px" }}>
+              <Player
+                playsInline
+                poster={Rectangle8}
+                src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+              >
+                <BigPlayButton position="center" />
+              </Player>
+            </Container>
+          </div>
+        </Container>
+        <Container maxWidth="md" className="span-6">
           <h2>Students</h2>
           <Typography variant="body2">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
